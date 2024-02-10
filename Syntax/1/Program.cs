8bc6532f0b2a8f55699eceb2879942791d7eccdf
mtucsni <159619716+mtucsni@users.noreply.github.com>
2024-02-10 16:46:37 +0200

    Add files via upload

---
+++ Syntax/1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the first number:");
+        int number1 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Enter the second number:");
+        int number2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine($"Before interchange: Number1 = {number1}, Number2 = {number2}");
+
+        // Swap the numbers
+        int temp = number1;
+        number1 = number2;
+        number2 = temp;
+
+        Console.WriteLine($"After interchange: Number1 = {number1}, Number2 = {number2}");
+    }
+}
---
+++ Syntax/2/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Enter the coefficient 'a' (a != 0):");
+        double a = Convert.ToDouble(Console.ReadLine());
+
+        while (a == 0)
+        {
+            Console.WriteLine("Coefficient 'a' cannot be zero. Please enter a non-zero value for 'a':");
+            a = Convert.ToDouble(Console.ReadLine());
+        }
+
+        Console.WriteLine("Enter the coefficient 'b':");
+        double b = Convert.ToDouble(Console.ReadLine());
+
+        // Solving the equation a*x + b = 0
+        double x = -b / a;
+
+        Console.WriteLine($"The solution of the equation {a}x + {b} = 0 is x = {x}");
+    }
+}
