b4c3005fd562d0a8838a3dc449b0a0353e609e8f
mtucsni <159619716+mtucsni@users.noreply.github.com>
2024-02-10 18:18:36 +0200

    Add files via upload

---
+++ Inheritance/1/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+public class Person
+{
+    public string Name { get; private set; }
+
+    public Person(string name)
+    {
+        Name = name;
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+public class Student : Person
+{
+    public Student(string name) : base(name) { }
+
+    public void Study()
+    {
+        Console.WriteLine($"{Name} is studying.");
+    }
+}
+
+public class Teacher : Person
+{
+    public Teacher(string name) : base(name) { }
+
+    public void Explain()
+    {
+        Console.WriteLine($"{Name} is explaining.");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        Person[] people = new Person[3];
+
+        Console.WriteLine("Enter the name of the first student:");
+        people[0] = new Student(Console.ReadLine());
+
+        Console.WriteLine("Enter the name of the second student:");
+        people[1] = new Student(Console.ReadLine());
+
+        Console.WriteLine("Enter the name of the teacher:");
+        people[2] = new Teacher(Console.ReadLine());
+
+        foreach (Person person in people)
+        {
+            if (person is Student student)
+            {
+                student.Study();
+            }
+            else if (person is Teacher teacher)
+            {
+                teacher.Explain();
+            }
+        }
+    }
+}
