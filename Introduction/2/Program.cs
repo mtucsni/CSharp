34bf1ba285274ea383fb875837b5c0cb0f6869e2
mtucsni <159619716+mtucsni@users.noreply.github.com>
2024-02-10 16:27:40 +0200

    Add files via upload

---
+++ Introduction/2/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter numbers (end with a number that ends in two zeros):");
+
+        while (true)
+        {
+            int number = Convert.ToInt32(Console.ReadLine());
+
+            numbers.Add(number);
+
+            // Check if the number ends in two zeros
+            if (number % 100 == 0)
+            {
+                break;
+            }
+        }
+
+        Console.WriteLine("Entered numbers:");
+        foreach (var num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
